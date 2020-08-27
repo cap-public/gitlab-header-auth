be7e57b8f156942a81edb5e90029ca911dbe1dae
Elisha Hastings <elishahastings@capuk.org>
2020-08-27 13:39:43 +0100

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule GitlabAuth.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.html": :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule GitlabAuth.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:excoveralls, "~> 0.13.1", only: :test},
     ]
   end
 end
