5270681725dbde8fcaf9497ab32dbd02478962f4
Elisha Hastings <elishahastings@capuk.org>
2020-09-30 11:59:20 +0100

    Add inch

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule GitlabHeaderAuth.MixProject do
       {:plug, "~> 1.10.4"},
       {:excoveralls, "~> 0.13.1", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:inch_ex, only: :docs},
     ]
   end
 
